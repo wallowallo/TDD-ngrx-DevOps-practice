6c7d78e5ef17f01871a3579cd8d7a0e3b90bf020
Krister <krister.torsvik@gmail.com>
2017-06-22 10:04:35 +0200

    added structure and ts lint preferances (#2)

---
+++ src/app/text-from-backend/text-from-backend.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-from-backend',
+  templateUrl: './text-from-backend.component.html',
+  styleUrls: ['./text-from-backend.component.css']
+})
+export class TextFromBackendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
